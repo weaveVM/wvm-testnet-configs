99d8572cec34c86d90c841959ede015678222c24
allnil <nil.medvedev17@gmail.com>
2024-08-09 12:29:40 +0100

    clean test scripts

--- handmade/generate_additional_validators.sh
+++ handmade/generate_additional_validators.sh
@@ -2,7 +2,9 @@
 
 rm -f validators.txt
 
-MNEMONIC="rose wheat tooth curtain leopard vivid agent six rose cricket mail extend deliver foot visual super potato clap witness rice spawn fan diary garment"
+MNEMONIC="mnemonic number one"
 eth2-val-tools deposit-data --fork-version 0x99000000 --source-max 1 --source-min 0 --validators-mnemonic="$MNEMONIC" --withdrawals-mnemonic="$MNEMONIC" --as-json-list | jq ".[] | \"0x\" + .pubkey + \":\" + .withdrawal_credentials + \":5000000000000\"" | tr -d '"' >> validators.txt
-MNEMONIC="laptop globe uncle crowd job uncle horse domain dentist stomach other huge silver offer despair general envelope capable slice marriage rack judge walnut brick" 
+
+
+MNEMONIC="mnemonic number two" 
 eth2-val-tools deposit-data --fork-version 0x99000000 --source-max 1 --source-min 0 --validators-mnemonic="$MNEMONIC" --withdrawals-mnemonic="$MNEMONIC" --as-json-list | jq ".[] | \"0x\" + .pubkey + \":\" + .withdrawal_credentials + \":5000000000000\"" | tr -d '"' >> validators.txt
