eab0a53d44a9707331f6b57d98536886fae8f87d
allnil <nil.medvedev17@gmail.com>
2024-08-09 17:41:50 +0100

    testnet: alphanet

--- handmade/generate_additional_validators.sh
+++ handmade/generate_additional_validators.sh
@@ -2,9 +2,11 @@
 
 rm -f validators.txt
 
-MNEMONIC="mnemonic number one"
-eth2-val-tools deposit-data --fork-version 0x99000000 --source-max 1 --source-min 0 --validators-mnemonic="$MNEMONIC" --withdrawals-mnemonic="$MNEMONIC" --as-json-list | jq ".[] | \"0x\" + .pubkey + \":\" + .withdrawal_credentials + \":5000000000000\"" | tr -d '"' >> validators.txt
+MNEMONIC="kiwi wet deer orchard column detect extra tooth bar pact extend lunch dream idle join cereal near success decorate shoulder grace cup public diet"
+eth2-val-tools deposit-data --fork-version 0x99000000 --source-max 43 --source-min 0 --validators-mnemonic="$MNEMONIC" --withdrawals-mnemonic="$MNEMONIC" --as-json-list | jq ".[] | \"0x\" + .pubkey + \":\" + .withdrawal_credentials + \":5000000000000\"" | tr -d '"' >> validators.txt
 
+MNEMONIC="memory mind rebel leg victory medal utility define success since flag aim barely clog recall napkin middle elephant utility throw muscle cool bitter history"
+eth2-val-tools deposit-data --fork-version 0x99000000 --source-max 43 --source-min 0 --validators-mnemonic="$MNEMONIC" --withdrawals-mnemonic="$MNEMONIC" --as-json-list | jq ".[] | \"0x\" + .pubkey + \":\" + .withdrawal_credentials + \":5000000000000\"" | tr -d '"' >> validators.txt
 
-MNEMONIC="mnemonic number two" 
-eth2-val-tools deposit-data --fork-version 0x99000000 --source-max 1 --source-min 0 --validators-mnemonic="$MNEMONIC" --withdrawals-mnemonic="$MNEMONIC" --as-json-list | jq ".[] | \"0x\" + .pubkey + \":\" + .withdrawal_credentials + \":5000000000000\"" | tr -d '"' >> validators.txt
+MNEMONIC="deny utility range retreat dentist find airport emerge rabbit result marine bike cactus museum canoe pluck never normal now hip ocean confirm toilet electric"
+eth2-val-tools deposit-data --fork-version 0x99000000 --source-max 42 --source-min 0 --validators-mnemonic="$MNEMONIC" --withdrawals-mnemonic="$MNEMONIC" --as-json-list | jq ".[] | \"0x\" + .pubkey + \":\" + .withdrawal_credentials + \":5000000000000\"" | tr -d '"' >> validators.txt
